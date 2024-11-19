7cac74bd933ed737a04d007af6f9bd8b4a25a465
Marcio <matiusbv@gmail.com>
2024-11-19 20:05:51 -0300

    3.0 Componente Página #2

--- eventos/apps/frontend/src/components/template/Pagina.tsx
+++ eventos/apps/frontend/src/components/template/Pagina.tsx
@@ -1,3 +1,5 @@
+import Logo from "./Logo";
+
 export interface PaginaProps {
     children: React.ReactNode;
     className?: string;
@@ -9,7 +11,11 @@ export default function Pagina(props: PaginaProps) {
             flex flex-col items-center py-10 min-h-screen
             bg-[url('/background.png')] bg-cover
         ">
-            <main className={props.className}>{props.children}</main>
+            <Logo />
+            <main className={`
+                flex-1 flex flex-col justify-center py-10    
+                container ${props.className} 
+            `}>{props.children}</main>
         </div>
     )
 }
